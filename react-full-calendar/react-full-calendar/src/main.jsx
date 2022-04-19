1394f6c8c916781f45368c408eab4e17b4876f03
Thabata Dornelas <97548859+dthabata@users.noreply.github.com>
2022-04-19 12:36:08 -0300

    Delete react-full-calendar directory

--- react-full-calendar/react-full-calendar/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import FullEventsCalendar from './FullEventsCalendar'
-
-document.addEventListener('DOMContentLoaded', function () {
-  createRoot(document.body.appendChild(document.createElement('div'))).render(
-    <FullEventsCalendar />
-  )
-})
