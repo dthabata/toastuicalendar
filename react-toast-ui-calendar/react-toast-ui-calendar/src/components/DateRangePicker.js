46bc2cb8e3a3724ae98b80b1652586d892701613
dthabata <thabata@bornlogic.com>
2022-05-03 14:38:30 -0300

    user test 2

--- react-toast-ui-calendar/react-toast-ui-calendar/src/components/DateRangePicker.js
+++ react-toast-ui-calendar/react-toast-ui-calendar/src/components/DateRangePicker.js
@@ -19,7 +19,7 @@ const TuiDateRangePicker = forwardRef((props, ref) => {
   const endPickerContainerRef = useRef(null)
   const endPickerInputRef = useRef(null)
 
-  /*   DatePicker.localeTexts['pt-br'] = {
+  DatePicker.localeTexts['pt-br'] = {
     titles: {
       // days
       DD: [
@@ -68,7 +68,7 @@ const TuiDateRangePicker = forwardRef((props, ref) => {
     todayFormat: 'D, ' + 'dd/MM,' + ' yyyy',
     date: 'Data',
     time: 'Hora',
-  } */
+  }
 
   useImperativeHandle(ref, () => ({
     setStartDate(start) {
